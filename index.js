3d938a8c3eeeaacef0a21442594c1ea2423ae992
Parker Preyer <parker.preyer@gmail.com>
2019-11-01 01:54:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 function theBeatlesPlay(musicians, instruments) {
   let arr = [];
 
-  for (var i = 0; i < musicians; i++) {
+  for (var i = 0; i < musicians.length; i++) {
     let musician = musicians[i];
     let instrument = instruments[i];
-    arr.push(musician + instrument);
+    arr.push(`${musician} plays ${instrument}`);
   };
   return arr;
 };
